5443017bb04d6757b354556d9a25883e31d81dc7
Bruno Bodian <bacarybruno@gmail.con>
2021-10-26 22:07:23 +0200

    chore: add port in env variable

--- src/main.ts
+++ src/main.ts
@@ -29,7 +29,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   await configureSwagger(app);
-  await app.listen(3000);
+  await app.listen(process.env.port || 3000);
 }
 
 bootstrap();
